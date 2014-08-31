0d1bfc6e21b8ec68ff73ee267a90de9c1e15b59f
Chris Mikelson <chris.mikelson@640labs.com>
2014-08-31 15:25:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Productmash::Application.config.secret_key_base = '18d0ef3a4b9a0a8cc30c126d99321094feb0a6897fe9ba2626502fa744bce6d73b09ef78704e9f85a4902dc48863d2a9c36494c28dc8134ea23e0dc055f4492b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Productmash::Application.config.session_store :cookie_store, key: '_productmash_session'
