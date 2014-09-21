83b2fe87a7b334bab95dcdbb978ca58de67dc281
Chris Mikelson <chris.mikelson@640labs.com>
2014-09-20 21:48:23 -0500

    Added provider

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140921020628) do
+ActiveRecord::Schema.define(version: 20140921024157) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140921020628) do
     t.string   "provider_id"
     t.string   "uid"
     t.string   "location"
+    t.string   "provider"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
