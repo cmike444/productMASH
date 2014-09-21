02d33e3de6e3ea8eb73672f2e45f937005038a93
Chris Mikelson <chris.mikelson@640labs.com>
2014-09-20 21:48:35 -0500

    Set up Facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, 
+         :omniauthable, :omniauth_providers => [:facebook]
+
+    def self.from_omniauth(auth)
+	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	    user.email = auth.info.email
+	    user.password = Devise.friendly_token[0,20]
+	    user.full_name = auth.info.name 
+	    user.first_name = auth.info.first_name
+	    user.last_name = auth.info.last_name 
+	    user.location = auth.info.location
+	    user.avatar = auth.info.image
+	  end
+	end
+
 end
