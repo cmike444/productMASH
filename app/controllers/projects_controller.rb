875b6966e9d73f53e8e4186928b203701b0a934a
Chris Mikelson <chris.mikelson@640labs.com>
2014-09-20 23:57:45 -0500

    scope projects to current user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,11 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+  layout 'dashboard'
 
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.where(:user_id => current_user.id).order('created_at desc')
   end
 
   # GET /projects/1
@@ -14,7 +15,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects/new
   def new
-    @project = Project.new
+    @project = current_user.projects.build
   end
 
   # GET /projects/1/edit
@@ -24,7 +25,7 @@ class ProjectsController < ApplicationController
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.new(project_params)
+    @project = current_user.projects.build(project_params)
 
     respond_to do |format|
       if @project.save
@@ -69,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params[:project]
+      params.require(:project).permit(:project, :image, :name, :description)
     end
 end
